8b88cf06dec5dce4660a10fd0fc2284344b6eb69
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-11-17 18:38:35 +0000

    added library class and passed has stock test

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> books;
+
+    public Library(){
+        this.books = new ArrayList<Book>();
+    }
+    public int getStockCount(){
+        return this.books.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+    private Library library;
+    private Book book;
+    @Before
+    public void before(){
+        this.library = new Library();
+        this.book = new Book("Children Of Time", "Adrian Tchaikovsky", "Sci-Fi");
+    }
+    @Test
+    public void libraryHasStock(){
+        assertEquals(0, this.library.getStockCount());
+    }
 }
